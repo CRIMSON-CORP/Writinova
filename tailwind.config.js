8cf6e0dc7bd5cac5e1055f259050af771527984d
Oluwatowo Rosanwo <olueatoworosanwo@gmail.com>
2023-07-10 17:43:11 +0100

    setting up stiles, header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 export default {
   content: ['./index.html'],
   theme: {
+    extend: {
+      colors: {
+        'primary-orange': '#FE7F3C',
+        'black-paragraph': '#494A4D',
+        'black-link': '#01020F',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '22px',
+          xl: '132px',
+          '2xl': '132px',
+        },
+      },
+      screens: {
+        xl: '1336px',
+        '2xl': '1440px',
+      },
+    },
   },
   plugins: [],
 };
